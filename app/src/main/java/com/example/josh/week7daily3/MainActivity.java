5bffb92e30bef5e76c14fb9170278a285bc07647
Joshua Robbins <joshua.robbins@mobileappscompany.us>
2018-12-05 23:28:00 -0500

    Initial commit

---
+++ app/src/main/java/com/example/josh/week7daily3/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.josh.week7daily3;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "_TAG";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void onClick(View v) {
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.initiateScan();
+    }
+
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        if (scanResult != null) {
+            Log.d(TAG, "onActivityResult: " + scanResult);
+        }
+    }
+
+    public void viewPDF(View view) {
+        File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath()+"/test2.pdf");
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setDataAndType(Uri.fromFile(file), "application/pdf");
+        intent.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+        startActivity(intent);
+    }
+}
